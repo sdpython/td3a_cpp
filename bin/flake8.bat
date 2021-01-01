00373941619fe939bfa726ddc938de10a10cbea2
xavier dupré <xavier.dupre@gmail.com>
2021-01-01 01:44:26 +0100

    update CI to python 3.9

--- bin/flake8.bat
+++ bin/flake8.bat
@@ -2,7 +2,7 @@
 set current=%~dp0
 set root=%current%..
 cd %root%
-set pythonexe="c:\Python372_x64\python.exe"
+set pythonexe="c:\Python391_x64\python.exe"
 if not exist %pythonexe% set pythonexe="c:\Python370_x64\python.exe"
 
 @echo running 'python -m flake8 td3a_cpp tests examples'
