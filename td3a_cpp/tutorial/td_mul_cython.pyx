c72a2591e6d0383806b789971b9621b4f6aa4e99
xavier dupré <xavier.dupre@gmail.com>
2021-01-19 18:55:26 +0100

    finalize the version

--- td3a_cpp/tutorial/td_mul_cython.pyx
+++ td3a_cpp/tutorial/td_mul_cython.pyx
@@ -20,14 +20,20 @@ def multiply_matrix(m1, m2):
     return m3
 
 
-cdef double[:, :] _c_multiply_matrix(double[:, :] m1, double[:, :] m2):
-    m3 = pynumpy.zeros((m1.shape[0], m2.shape[1]), dtype=pynumpy.float64)
-    for i in range(0, m1.shape[0]):
-        for j in range(0, m2.shape[1]):
-            for k in range(0, m1.shape[1]):
+cdef void _c_multiply_matrix(double[:, :] m1, double[:, :] m2,
+                             double[:, :] m3,
+                             cython.int ni, cython.int nj, cython.int nk) nogil:
+    cdef cython.int i, j, k    
+    for i in prange(0, ni):
+        for j in range(0, nj):
+            for k in range(0, nk):
                 m3[i, j] += m1[i, k] * m2[k, j]
-    return m3
 
 
 def c_multiply_matrix(double[:, :] m1, double[:, :] m2):
-    return _c_multiply_matrix(m1, m2)
+    m3 = pynumpy.zeros((m1.shape[0], m2.shape[1]), dtype=pynumpy.float64)
+    cdef cython.int ni = m1.shape[0]
+    cdef cython.int nj = m2.shape[1]
+    cdef cython.int nk = m1.shape[1]
+    _c_multiply_matrix(m1, m2, m3, ni, nj, nk)
+    return m3
--- tests/test_tutorial_td.py
+++ tests/test_tutorial_td.py
@@ -1,10 +1,12 @@
 """
 """
 import unittest
+import timeit
 import numpy
 from numpy.testing import assert_almost_equal
 from td3a_cpp.tutorial.td_mul_cython import (
     multiply_matrix, c_multiply_matrix)
+from td3a_cpp.tutorial.mul_cython_omp import dmul_cython_omp
 
 
 class TestTutorialTD(unittest.TestCase):
@@ -22,6 +24,19 @@ class TestTutorialTD(unittest.TestCase):
         res1 = va @ vb
         res2 = c_multiply_matrix(va, vb)
         assert_almost_equal(res1, res2)
+
+    def test_timeit(self):
+        va = numpy.random.randn(300, 400).astype(numpy.float64)
+        vb = numpy.random.randn(400, 500).astype(numpy.float64)
+        res1 = va @ vb
+        res2 = c_multiply_matrix(va, vb)
+        ctx = {'va': va, 'vb': vb, 'c_multiply_matrix': c_multiply_matrix}
+        res1 = timeit.timeit('va @ vb', number=10, globals=ctx)
+        res2 = timeit.timeit('c_multiply_matrix(va, vb)', number=10, globals=ctx)
+        self.assertLess(res1, res2)  # numpy is much faster.
+        ratio = res2 / res1
+        self.assertGreater(ratio, 1)  # ratio = number of times numpy is faster
+        # print(ratio)
 
 
 if __name__ == '__main__':
