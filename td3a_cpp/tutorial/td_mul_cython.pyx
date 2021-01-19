7d6928405186c8799db37eb2173dd404fd0d3f70
xavier dupré <xavier.dupre@gmail.com>
2021-01-19 17:49:02 +0100

    Add cython function

--- td3a_cpp/tutorial/td_mul_cython.pyx
+++ td3a_cpp/tutorial/td_mul_cython.pyx
@@ -19,3 +19,15 @@ def multiply_matrix(m1, m2):
                 m3[i, j] += m1[i, k] * m2[k, j]
     return m3
 
+
+cdef double[:, :] _c_multiply_matrix(double[:, :] m1, double[:, :] m2):
+    m3 = pynumpy.zeros((m1.shape[0], m2.shape[1]), dtype=pynumpy.float64)
+    for i in range(0, m1.shape[0]):
+        for j in range(0, m2.shape[1]):
+            for k in range(0, m1.shape[1]):
+                m3[i, j] += m1[i, k] * m2[k, j]
+    return m3
+
+
+def c_multiply_matrix(double[:, :] m1, double[:, :] m2):
+    return _c_multiply_matrix(m1, m2)
--- tests/test_tutorial_td.py
+++ tests/test_tutorial_td.py
@@ -3,7 +3,8 @@
 import unittest
 import numpy
 from numpy.testing import assert_almost_equal
-from td3a_cpp.tutorial.td_mul_cython import multiply_matrix
+from td3a_cpp.tutorial.td_mul_cython import (
+    multiply_matrix, c_multiply_matrix)
 
 
 class TestTutorialTD(unittest.TestCase):
@@ -13,6 +14,13 @@ class TestTutorialTD(unittest.TestCase):
         vb = numpy.random.randn(4, 5).astype(numpy.float64)
         res1 = va @ vb
         res2 = multiply_matrix(va, vb)
+        assert_almost_equal(res1, res2)
+
+    def test_matrix_cmultiply_matrix(self):
+        va = numpy.random.randn(3, 4).astype(numpy.float64)
+        vb = numpy.random.randn(4, 5).astype(numpy.float64)
+        res1 = va @ vb
+        res2 = c_multiply_matrix(va, vb)
         assert_almost_equal(res1, res2)
 
 
